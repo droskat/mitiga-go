cab58478be6a8247e73cb028826e6594ca50f01b
Dennish Jose <dennish.jose>
2026-04-01 14:59:04 +0530

    frontend docker change for connection to backend

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-envsubst '${BACKEND_PORT}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '${BACKEND_HOST} ${BACKEND_PORT}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
 exec nginx -g 'daemon off;'
